e3e7de2ab219a0fb9b1db2e44ea0428556c9b27b
Everton da Rosa <everton3x@gmail.com>
2026-01-13 10:41:07 -0300

    Atualizado teste AccountingTestingPlatform.Test.TypeTests.ConsistencyTest.ConcreteTests.CreditoEmpenhadoALiquidarTest
    No encerramento de dezembro, não estava considerando a troca de contas para os restos a pagar.

--- AccountingTestingPlatform/Test/TypeTests/ConsistencyTest/ConcreteTests/CreditoEmpenhadoALiquidarTest.cs
+++ AccountingTestingPlatform/Test/TypeTests/ConsistencyTest/ConcreteTests/CreditoEmpenhadoALiquidarTest.cs
@@ -19,6 +19,8 @@ namespace AccountingTestingPlatform.Test.TypeTests.ConsistencyTest.ConcreteTests
         {
             string[] sql1 = [
                 $"select sum(saldo_atual)::decimal from pad.bal_ver where remessa = {_remessa} and entidade like '{entidade}' and escrituracao like 'S' and conta_contabil like '6221301%';",
+                $"select sum(saldo_atual)::decimal from pad.bal_ver where remessa = {_remessa} and entidade like '{entidade}' and escrituracao like 'S' and conta_contabil like '6221305%';",
+                $"select sum(saldo_atual)::decimal from pad.bal_ver where remessa = {_remessa} and entidade like '{entidade}' and escrituracao like 'S' and conta_contabil like '6221306%';",
             ];
             string[] sql2 = [
                 $"select sum(empenhado_a_liquidar)::decimal from pad.bal_desp where remessa = {_remessa} and entidade like '{entidade}';",
